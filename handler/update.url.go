50380d29394db2f9ee8b8fbcbddfe16a19c6a046
Bambang Handoko <bambang.handoko12@gmail.com>
2022-10-21 20:28:36 +0700

    update url api done

--- handler/update.url.go
+++ handler/update.url.go
@@ -21,17 +21,51 @@ func UpdateURLByID(w http.ResponseWriter, r *http.Request) {
 	var url entity.URLshortener
 	json.Unmarshal(body, &url)
 
-	result := database.Connector.Debug().Table(url.TableName()).
-		Select("original_url", "redirect_count").
+	result := database.Connector.Table(url.TableName()).
 		Where("id = ?", ID).
-		Update(&url)
+		Update(map[string]interface{}{
+			"original_url":   url.OriginalURL,
+			"redirect_count": url.RedirectCount,
+		})
 
 	if result.Error != nil {
 		log.Fatalln(result.Error.Error())
 	}
-	// json.NewEncoder(w).Encode(url)
+
+	result.Scan(&url)
+	if result.RowsAffected > 0 {
+		json.NewEncoder(w).Encode(url)
+	} else {
+		json.NewEncoder(w).Encode("Update url failed")
+	}
+
 }
 
 func UpdateURLByUniqueCode(w http.ResponseWriter, r *http.Request) {
-	panic("implement me!")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	uniqueCode := params["uniqueCode"]
+
+	body, _ := ioutil.ReadAll(r.Body)
+	var url entity.URLshortener
+	json.Unmarshal(body, &url)
+
+	result := database.Connector.Debug().Table(url.TableName()).
+		Where("unique_code = ?", uniqueCode).
+		Update(map[string]interface{}{
+			"original_url":   url.OriginalURL,
+			"redirect_count": url.RedirectCount,
+		})
+
+	if result.Error != nil {
+		log.Fatalln(result.Error.Error())
+	}
+
+	result.Scan(&url)
+	if result.RowsAffected > 0 {
+		json.NewEncoder(w).Encode(url)
+	} else {
+		json.NewEncoder(w).Encode("Update url failed")
+	}
 }
